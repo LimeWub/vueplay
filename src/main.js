10e75dd9f9667debfba7b1b826719cf048201003
LimeWub <myrto.kontouli@gmail.com>
2018-01-16 21:12:45 +0000

    Day3: Re-adding vuetify. Fixing some of the things I broke with the re-install...

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import Vuetify from 'vuetify';
+ 
+Vue.use(Vuetify);
+import('../node_modules/vuetify/dist/vuetify.min.css');
 
 Vue.config.productionTip = false;
 
