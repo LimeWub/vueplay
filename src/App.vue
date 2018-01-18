0e4e9a915276a53ca0ab3a5559bb0420e9ef08fc
LimeWub <myrto.kontouli@gmail.com>
2018-01-18 22:45:02 +0000

    Day4: Started tutorial for Vuetify. Stuck due to glitch(?) Need to investigate.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app >
+    <v-navigation-drawer fixed v-model="siteNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>code</v-icon> 
+          </v-list-tile-action>
+          <v-list-tile-content> 
+            <v-list-tile-title> Link 1</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-toolbar>
+      <v-toolbar-title> 
+        Hello Vue
+      </v-toolbar-title>
+      <v-toolbar-side-icon @click="siteNav = !siteNav"></v-toolbar-side-icon>
+      <v-spacer> </v-spacer>
+      <v-toolbar-items>
+        <v-btn flat><v-icon left>code</v-icon> Link 1 </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+       <router-view/>
+    </main> 
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data () {
+    return {
+      siteNav: false
+    }
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
